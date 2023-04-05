ef4f22d600a41c17e907353e58608517263a866b
kimng1003 <kimnagyeong50@gmail.com>
2023-04-05 22:42:46 +0900

    chapter04.level2Test

---
+++ src/com/javaStudy/ch4/level02/basicAp1.java
@@ -0,0 +1,19 @@
+package com.javaStudy.ch4.level02;
+
+public class basicAp1 {
+
+	public static void main(String[] args) {
+		
+		/* 1부터 10까지 합계를 구하고 결과를 출력하세요
+		 * 
+		 * -- 출력 예시 --
+		 * 1부터 10까지의 합 : 55
+		 * */
+
+		int sum = 0;
+		for(int i = 1; i <11; i++) {
+			sum += i;
+		}
+		 System.out.println("1부터 10까지 합계: " + sum);
+	}
+}
---
+++ src/com/javaStudy/ch4/level02/basicAp2.java
@@ -0,0 +1,16 @@
+package com.javaStudy.ch4.level02;
+
+public class basicAp2 {
+	public static void main(String[] args) {
+	
+	/* 10부터 1까지 결과를 출력하세요
+	 * 
+	 * -- 출력 예시 --
+	 * 10 9 8 7 6 5 4 3 2 1
+	 * */
+		
+	for(int i = 10; i > 0; i--) {
+		System.out.println(i + " ");
+	}
+	}
+}
